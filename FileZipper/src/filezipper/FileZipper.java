191b6ce5f6f99613e4344a8641023afd55f2d475
Jakub Krawczyk <40919861+thejamesK@users.noreply.github.com>
2018-08-28 14:14:27 +0200

    Upgraded Zip Button
    Added Zip option, now program is complete

--- FileZipper/src/filezipper/FileZipper.java
+++ FileZipper/src/filezipper/FileZipper.java
@@ -2,8 +2,15 @@ package filezipper;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import javax.swing.*;
 import sun.font.LayoutPathImpl;
 
@@ -128,7 +135,7 @@ public class FileZipper extends JFrame
             else if(ae.getActionCommand().equals("Delete"))
                 deleteFilesFromZip();
             else if(ae.getActionCommand().equals("Zip"))
-                System.out.println("Zip");
+                makeZipFile();
            
         }
         
@@ -171,6 +178,87 @@ public class FileZipper extends JFrame
             for(int i = 0; i < tmp.length; i++)
                 listModel.remove(tmp[i]-i);
         }
+        
+        private void makeZipFile()
+        {
+            chooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+            chooser.setSelectedFile(new File(System.getProperty("user.dir")+File.separator+"*.zip"));
+            int tmp = chooser.showDialog(rootPane, "Make Zip!");
+            
+            if(tmp == JFileChooser.APPROVE_OPTION)
+            {
+            
+                byte tmpData[] = new byte[BUFFOR];
+                try 
+                {
+                    ZipOutputStream zOutS = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(chooser.getSelectedFile()), BUFFOR));
+
+                    for(int i = 0; i < listModel.getSize(); i++)
+                    {
+                        if(!((File)listModel.get(i)).isDirectory())
+                            zip(zOutS, (File)listModel.get(i), tmpData, ((File)listModel.get(i)).getPath());
+                        else
+                        {
+                            printPaths((File)listModel.get(i));
+                            
+                            for (int j = 0; j < pathList.size(); j++)
+                                zip(zOutS, (File)pathList.get(j), tmpData, ((File)listModel.get(i)).getPath());
+                            
+                            pathList.removeAll(pathList);
+                        }
+                        
+                    }
+
+                    zOutS.close();
+                } 
+                catch (IOException ex) 
+                {
+                    System.out.println(ex.getMessage());
+                }
+            
+            }
+            
+            
+        }
+        private void zip(ZipOutputStream zOutS, File filePath, byte[] tmpData, String defaultPath) throws IOException
+        {
+            BufferedInputStream inS = new BufferedInputStream(new FileInputStream(filePath), BUFFOR);
+
+            zOutS.putNextEntry(new ZipEntry(filePath.getPath().substring(defaultPath.lastIndexOf(File.separator)+1)));
+
+            int counter;
+            while ((counter = inS.read(tmpData, 0, BUFFOR)) != -1)
+                zOutS.write(tmpData, 0, counter);
+
+
+            zOutS.closeEntry();
+
+            inS.close();
+        }
+        public static final int BUFFOR = 1024;
+        
+        private void printPaths(File pathName)
+        {
+            String[] fileAndDirectoryName = pathName.list();
+            
+            for(int i = 0; i < fileAndDirectoryName.length; i++)
+            {
+                File p = new File(pathName.getPath(), fileAndDirectoryName[i]);
+
+
+                if(p.isFile())
+                    pathList.add(p);
+
+
+                if(p.isDirectory())
+                {
+                    printPaths(new File (p.getPath()));
+
+                }
+            }
+        }
+        
+        ArrayList pathList = new ArrayList();
     }
         
 }
